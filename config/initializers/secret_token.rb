9a438ad57f7fe42d29af8d24c47e84993bd864f8
Matt Evans <matt@thebetacollective.co.uk>
2013-06-04 17:16:29 +0100

    Rails folder created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journaly::Application.config.secret_token = 'bd98b12561113adb86269ee302147235956a7c68cc11e4991d47c5318b0e52e0aab243d6a9b023006b27c0a5a252c0dc53503c66e9c5bdf2b05cf8b5c2518540'
