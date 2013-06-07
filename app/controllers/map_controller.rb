400b19e65d19f94020fa8a29d19c8b7e73f71c0b
Matt Evans <matt@thebetacollective.co.uk>
2013-06-07 15:03:57 +0100

    Integration with maps

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,6 @@ class MapController < ApplicationController
 
 
 	def index
-		puts Trip.find(params[:id])
 		@place = Trip.find(params[:id])
 		@trip = Trip.find(params[:id])
 		latitude = @trip.places.find(params[:latitude])
@@ -10,23 +9,16 @@ class MapController < ApplicationController
 	end
 
 	def show
-
+		@trip = Trip.find(params[:id])
 		@place = Trip.find(params[:id]).places
-		puts @place.inspect
-
 		@markerData = []
 		@place.each_index do |x|
-
-		 @markerData << {"name"=> @place[x].place, "longitude"=> @place[x].longitude, "latitude"=> @place[x].latitude}
-		 end
-
-		 puts @markerData
-
-		 @markerData = @markerData.to_json
-
-		 respond_to do |format|
-		 	format.html
-		 	format.json {render json: @markerData}
+			@markerData << {"name"=> @place[x].place, "longitude"=> @place[x].longitude, "latitude"=> @place[x].latitude}
+		end
+		@markerData = @markerData.to_json
+		respond_to do |format|
+			format.html
+			format.json {render json: @markerData}
 		end
 	end
 
