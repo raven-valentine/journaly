9beb231b200cedde00dc1b47cd81e01260b110de
Keir Osborn <keir.osborn@gmail.com>
2013-06-13 11:26:05 +0100

    changed the places migration

--- db/migrate/20130605101707_create_places.rb
+++ db/migrate/20130605101707_create_places.rb
@@ -2,8 +2,8 @@ class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
       t.string :place
-      t.string :start_date
-      t.string :end_date
+      t.date :start_date
+      t.date :end_date
       t.string :latitude
       t.string :longitude
       t.timestamps
