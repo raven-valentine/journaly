36c6032ab6f5807b13b2d7b95d737097dcf44e25
Matt Evans <matt@thebetacollective.co.uk>
2013-06-14 16:56:49 +0100

    Created Use passing rspec test with tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,10 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :password_digest
+  attr_accessible :name, :password, :password_confirmation
   has_secure_password
 
   validates :name, presence: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,47 +1,107 @@
 require 'spec_helper'
 
+# describe User do
+
+#   let(:user) { User.new(name: "Matt")}
+#   let(:user_two) { User.new(name: "")}
+#   let(:user_three) {User.create(name: "Matt")}
+
+#   it 'has a name' do
+#     user.should respond_to(:name)
+#   end
+
+#   it "when a name is not present" do
+#     user_two.should_not be_valid
+#   end
+
+#   # it "when name is already taken" do
+#   #   user_three.should_not be_valid
+#   # end
+
+#   context "security" do
+
+#     it "has a password digest" do
+#       user.should respond_to(:password_digest)
+#     end
+
+#     it "has a password" do
+#       user.should respond_to(:password)
+#     end
+
+#     it "has a password confirmation" do
+#       user.should respond_to(:password_confirmation)
+#     end
+
+#     it "password_confirmation should not be blank" do
+#       user.password = user.password_confirmation = " "
+#       user.should_not be_valid
+#     end
+
+#     it "password_confirmation should not be a mismatch" do
+#       user.password_confirmation = "mismatch"
+#       user.should_not be_valid
+#     end
+
+#   end
+# end
+
 describe User do
 
-  let(:user) { User.new(name: "Matt")}
-  let(:user_two) { User.new(name: "")}
-  let(:user_three) {User.create(name: "Matt")}
-
-  it 'has a name' do
-    user.should respond_to(:name)
+  before do
+    @user = User.new(name: "Example User",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
-  it "when a name is not present" do
-    user_two.should_not be_valid
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
   end
 
-  # it "when name is already taken" do
-  #   user_three.should_not be_valid
-  # end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
-  context "security" do
+  describe "when password confirmation is nil" do
+    before { @user.password_confirmation = nil }
+    it { should_not be_valid }
+  end
 
-    it "has a password digest" do
-      user.should respond_to(:password_digest)
+  describe "return value of authenticate method" do
+    before { @user.save}
+    let (:found_user) { User.find_by_name(@user.name)}
+
+    describe "with valid password" do
+      it {should == found_user.authenticate(@user.password)}
     end
 
-    it "has a password" do
-      user.should respond_to(:password)
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
     end
 
-    it "has a password confirmation" do
-      user.should respond_to(:password_confirmation)
-    end
-
-    it "password_confirmation should not be blank" do
-      user.password = user.password_confirmation = " "
-      user.should_not be_valid
-    end
-
-    it "password_confirmation should not be a mismatch" do
-      user.password_confirmation = "mismatch"
-      user.should_not be_valid
+    describe "with a password that is too short" do
+      before { @user.password = @user.password_confirmation = "a" * 5}
+      it { should be_invalid}
     end
 
   end
-
 end
+
+
+
+
+
+
